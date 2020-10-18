aa87ff97581b6d6fb8c9e580f90b821774129e89
MariamaB <s0539148@htw-berlin.de>
2020-10-18 17:17:08 +0200

    remove not needed components for presenation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
-import { BmcanvasListViewComponent } from './components/bmcanvas-list-view/bmcanvas-list-view.component';
-import { BmcanvasViewComponent } from './components/bmcanvas-view/bmcanvas-view.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { QuestionnaireComponent } from './components/questionnaire-view/questionnaire-view.component';
 import { TustBuildCanvasViewComponent } from './components/tust-build-canvas-view/tust-build-canvas-view.component';
@@ -16,14 +14,6 @@ const routes: Routes = [
     path: '',
     redirectTo: 'questionnaire',
     pathMatch: 'full',
-  },
-  {
-    path: 'bmcanvas-list',
-    component: BmcanvasListViewComponent,
-  },
-  {
-    path: 'bmcanvas',
-    component: BmcanvasViewComponent,
   },
   {
     path: 'questionnaire',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,9 @@ import { QuestionnaireComponent } from './components/questionnaire-view/question
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { BmcanvasListViewComponent } from './components/bmcanvas-list-view/bmcanvas-list-view.component';
 import { GraphQLModule } from './graphql/graphql.module';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BmcanvasViewComponent } from './components/bmcanvas-view/bmcanvas-view.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { HomeComponent } from './components/home/home.component';
 import { MaterialModule } from './material/material.module';
@@ -18,8 +16,6 @@ import { TustBuildCanvasViewComponent } from './components/tust-build-canvas-vie
 @NgModule({
   declarations: [
     AppComponent,
-    BmcanvasListViewComponent,
-    BmcanvasViewComponent,
     PageNotFoundComponent,
     HomeComponent,
     QuestionnaireComponent,
