067387e9e4b6ab2aa8a6c971df6fe7007f07e058
MariamaB <s0539148@htw-berlin.de>
2021-03-10 13:01:42 +0100

    include puppeteer.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { MediaObserver, MediaChange } from '@angular/flex-layout';
+const puppeteer = require('puppeteer');
 
 @Component({
   selector: 'app-root',
@@ -35,6 +36,34 @@ export class AppComponent {
     // this.headline = 'Trust-Build-Canvas';
   }
 
+  public async saveAsPDF() {
+    const browser = await puppeteer.launch();
+    // const browser = await puppeteer.launch({headless: false});
+    const page = await browser.newPage();
+    await page.emulateMediaType('screen');
+    await page.setViewport({ width: 1980, height: 1100 });
+    await page.goto('http://localhost:4200/trust-build-canvas', {
+      waitUntil: 'networkidle0',
+    });
+    await page.screenshot({ path: 'canvas.png' });
+    await page.emulateMediaType('screen');
+    await page.pdf({
+      printBackground: true,
+      path: 'webpage.pdf',
+      format: 'A4',
+      landscape: true,
+      //     margin: {
+      //         top: "20px",
+      //         bottom: "40px",
+      //         left: "20px",
+      //         right: "20px"
+      //     }
+    });
+    console.log('done');
+    // await browser.waitForTarget(()=> false);
+    await browser.close();
+    await process.exit();
+  }
   ngOnDestroy(): void {
     this.mediaSub.unsubscribe();
   }
